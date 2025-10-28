79371b5a5929fc0952374ce1cda55401d430a98d
Bowen Jiang <120222039+Wize998@users.noreply.github.com>
2025-10-27 22:41:37 -0400

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -14,19 +14,50 @@ from Triangle import classifyTriangle
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
+class TriangleTests(unittest.TestCase):
+    # ----- valid triangles -----
+    def test_equilateral(self):
+        self.assertEqual(classifyTriangle(7, 7, 7), "Equilateral")
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def test_isosceles_all_permutations(self):
+        for a, b, c in [(5, 5, 3), (5, 3, 5), (3, 5, 5)]:
+            with self.subTest(sides=(a, b, c)):
+                self.assertEqual(classifyTriangle(a, b, c), "Isosceles")
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def test_scalene(self):
+        self.assertEqual(classifyTriangle(4, 5, 6), "Scalene")
 
-if __name__ == '__main__':
-    print('Running unit tests')
+    def test_right_345_all_orders(self):
+        for a, b, c in [(3, 4, 5), (5, 3, 4), (4, 5, 3)]:
+            with self.subTest(sides=(a, b, c)):
+                self.assertEqual(classifyTriangle(a, b, c), "Right")
+
+    def test_right_5_12_13(self):
+        self.assertEqual(classifyTriangle(5, 12, 13), "Right")
+
+    def test_not_triangle_equality_boundary(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), "NotATriangle")
+
+    def test_not_triangle_strict(self):
+        self.assertEqual(classifyTriangle(2, 3, 6), "NotATriangle")
+
+    def test_zero_and_negative(self):
+        bads = [(0, 4, 5), (4, 0, 5), (4, 5, 0),
+                (-1, 2, 2), (2, -1, 2), (2, 2, -1)]
+        for s in bads:
+            with self.subTest(sides=s):
+                self.assertEqual(classifyTriangle(*s), "InvalidInput")
+
+    def test_non_integers(self):
+        for s in [(3.5, 4, 5), ("3", 4, 5), (True, 4, 5)]:
+            with self.subTest(sides=s):
+                self.assertEqual(classifyTriangle(*s), "InvalidInput")
+
+    # common spec guard: sides must be <= 200
+    def test_too_large_values(self):
+        for s in [(201, 10, 10), (10, 201, 10), (10, 10, 201)]:
+            with self.subTest(sides=s):
+                self.assertEqual(classifyTriangle(*s), "InvalidInput")
+
+if __name__ == "__main__":
     unittest.main()
-
